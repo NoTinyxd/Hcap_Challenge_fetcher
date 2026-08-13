a115f75a6b4b30c8ad9c2f5008d71acf009902be
NoTinyxd <160841329+NoTinyxd@users.noreply.github.com>
2026-08-14 00:20:36 +0500

    Refactor main.py for improved structure and readability
    Refactor code to encapsulate functionality in functions and improve readability. Added js_execute and check_config functions for better structure.

--- main.py
+++ main.py
@@ -3,80 +3,85 @@ import re
 import base64
 import json
 from playwright.sync_api import sync_playwright
-sitekey = "a9b5fb07-92ff-493f-86fe-352a2803b3df"
-headers = {
-    "accept": "application/json",
-    "accept-language": "en-US,en;q=0.9",
-    # 'content-length': '0',
-    "content-type": "text/plain",
-    "origin": "https://newassets.hcaptcha.com",
-    "priority": "u=1, i",
-    "referer": "https://newassets.hcaptcha.com/",
-    "sec-ch-ua": '"Google Chrome";v="147", "Not.A/Brand";v="8", "Chromium";v="147"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": '"Windows"',
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-site",
-    "sec-fetch-storage-access": "active",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36",
-}
-r = requests.get("https://hcaptcha.com/1/api.js")
-version = re.search(r"v=([a-f0-9]+)", r.text).group(1)
-print(f"version: {version}")
-params = {
-    "v": version,
-    "host": "discord.com",
-    "sitekey": sitekey,
-    "sc": "1",
-    "swa": "1",
-    "spst": "1",
-}
+def check_config(sitekey):
+    #sitekey = "a9b5fb07-92ff-493f-86fe-352a2803b3df"
+    headers = {
+        "accept": "application/json",
+        "accept-language": "en-US,en;q=0.9",
+        # 'content-length': '0',
+        "content-type": "text/plain",
+        "origin": "https://newassets.hcaptcha.com",
+        "priority": "u=1, i",
+        "referer": "https://newassets.hcaptcha.com/",
+        "sec-ch-ua": '"Google Chrome";v="147", "Not.A/Brand";v="8", "Chromium";v="147"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+        "sec-fetch-storage-access": "active",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36",
+    }
+    r = requests.get("https://hcaptcha.com/1/api.js")
+    version = re.search(r"v=([a-f0-9]+)", r.text).group(1)
+    print(f"version: {version}")
+    params = {
+        "v": version,
+        "host": "discord.com",
+        "sitekey": sitekey,
+        "sc": "1",
+        "swa": "1",
+        "spst": "1",
+    }
 
-response = requests.post(
-    "https://api.hcaptcha.com/checksiteconfig", params=params, headers=headers
-)
-# print(response.json())
-req = response.json()[
-    "c"
-][
-    "req"
-]  # so we get the middle value which is payload , btw there there three parts in that req first one header , middle one is payload and last one is signature
+    response = requests.post(
+        "https://api.hcaptcha.com/checksiteconfig", params=params, headers=headers
+    )
+    # print(response.json())
+    req = response.json()[
+        "c"
+    ][
+        "req"
+    ]  # so we get the middle value which is payload , btw there there three parts in that req first one header , middle one is payload and last one is signature
+    print(req)
+    payload = req.split(".")[1]
+    payload += "=" * (4 - len(payload) % 4)
+
+    decoded = json.loads(base64.b64decode(payload))
+    print(f"Decoded the payload{decoded}")
+    c = response.json()["c"]
+    l = decoded["l"]
+    return l,req,version,c
+
+
+sitekey ="a9b5fb07-92ff-493f-86fe-352a2803b3df"
+l,req,version,c = check_config(sitekey)
+print(l)
 print(req)
-payload = req.split(".")[1]
-payload += "=" * (4 - len(payload) % 4)
+def js_link(js:str):
+    url = f"https://newassets.hcaptcha.com" + l + "/"+ str(js)
+    print(url)
+    url_t = requests.get(url).text
+    #print("got hsw js length:", len(url_t))  # print the length of hsw.js
+    return url_t
 
-decoded = json.loads(base64.b64decode(payload))
-print(f"Decoded the payload{decoded}")
-c = response.json()["c"]
-l = decoded["l"]
-# print(l)
-print(c)
-hsw_url = "https://newassets.hcaptcha.com" + l + "/hsw.js"
-print(hsw_url)
-hsw = requests.get(hsw_url).text
-print("got hsw js length:", len(hsw))  # print the length of hsw.js
+url_t=js_link("hsw.js")
+
+def js_execute(url_t):
 
 
-# hsw stuff
 
+    from camoufox.sync_api import Camoufox
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)
-    page = browser.new_page()
-    page.evaluate(hsw)  # we store the loaded hsw function
-    n_val = page.evaluate(f'hsw("{req}")')  # get n value
-    print(f"result {n_val}")  # print n value
-#or use camoufox instead of playwright
-# from camoufox.sync_api import Camoufox
-
-# with Camoufox(headless=True) as browser:
-#     page = browser.new_page()
-#     page.evaluate(hsw)
-#     n_val = page.evaluate(f'hsw("{req}")')
-#     print(f"result {n_val}")
-
+    with Camoufox(headless=True) as browser:
+        page = browser.new_page()
+        page.evaluate(url_t)
+        n_val = page.evaluate(f'hsw("{req}")')
+        print(f"result {n_val}")
+        return(n_val)
 # get cap
+
+n_val=js_execute(url_t)
 
 h = {
     "accept": "application/json",
@@ -115,5 +120,4 @@ getcap = requests.post(
 )
 
 print(getcap.status_code)
-#print(getcap.headers.get("content-type")) keep it for debugging purposes
 print(getcap.json())
